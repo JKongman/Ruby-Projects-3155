6f75637c7fa73fd86ee31d8961f3b7a36a0dd2a8
Justin <Jkongman@uncc.edu>
2019-01-31 16:12:22 +0000

    RubyBasics1 - Part 3 V1

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -12,21 +12,31 @@ end
 # Part II
 def max_2_sum arr
   # YOUR CODE HERE
-  def max_2_sum(array)
-  case array.length
+  def max_2_sum(arr)
+      max = 0
+  case arr.length
    when 0
      return 0
      when 1
-       return array.first
+       return arr.first
      else
-       array.sort!
+       arr.sort!
        max = arr[-1] + arr[-2]
        return max
   end
-
+  end
+end
   
 
 # Part III
 def sum_to_n? arr, n
   # YOUR CODE HERE
+if arr.empty?
+    return false if n == 0
+else
+    arr.combination(2).to_a.each do |pair|
+        return true if sum(pair) == n
+    end
+end
+    return false
 end
