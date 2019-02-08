a847060cf8417d7073561639566b8ce2745072e0
Justin <Jkongman@uncc.edu>
2019-02-08 03:27:50 +0000

    Ruby Basics 2 - Part 3

--- RubyBasics2/lib/rubybasics2.rb
+++ RubyBasics2/lib/rubybasics2.rb
@@ -24,4 +24,30 @@ end
 # Part III
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if s.class == String 
+    if s.count('01') != s.size || s.length < 1
+      return false
+    elsif /^[10]*00$/.match(s) || s == "0"
+      return true
+    else
+      return false
+    end
+  else
+    binstr = s.to_s
+    if binstr.count('01') != binstr.size
+        if s.to_s(8).count('01') == s.to_s(8).size
+            binstr = s.to_s(8)
+            binary_multiple_of_4?(binstr)
+        end
+    elsif binstr.count('01') == binstr.size
+      if /^[10]*00$/.match(binstr)
+        return true
+      else
+        return false
+      end
+    else
+      puts "'#{s}' is not a valid binary number!"
+      return false
+    end
+  end
 end
